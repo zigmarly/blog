8b40b93634b858d45fea2db5c1123cad79a409a0
Zan Khan <Zankhan@me.com>
2018-05-21 20:34:27 -0600

    pages feature

---
+++ db/migrate/20180522003941_create_pages.rb
@@ -0,0 +1,11 @@
+class CreatePages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.string :author
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
